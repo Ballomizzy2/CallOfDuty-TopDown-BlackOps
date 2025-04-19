000e9207b9f9d654b51d6669a4b1f8a76a8fe676
TunedTuna <143358621+TunedTuna@users.noreply.github.com>
2025-04-19 14:59:15 -0700

    perk stuff
    -perks scriptable object
    -script for machine to hold soda SO
    -PerkMachineHandler will probbly be in player for logic?

---
+++ Assets/Scripts/PerkMachineHandler.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class PerkMachineHandler : MonoBehaviour
+{
+    //player interact event
+    //[SerializeField] private 
+    //when player raycasts, it checks if player has. if yes, gui n can buy. else no gui and no buy
+}
---
+++ Assets/Scripts/ScriptableObjects/PerkSodasSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class PerkSodasSO : ScriptableObject
+{
+    public string perkName;
+    public int price;
+    public float modifier;
+
+}
