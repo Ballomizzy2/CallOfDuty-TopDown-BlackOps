886493552210a72a1d90df481c32a69b0326ae41
TunedTuna <143358621+TunedTuna@users.noreply.github.com>
2025-04-28 19:33:12 -0700

    simple door prefab

---
+++ Assets/Scripts/DoorsHandler.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class DoorsHandler : MonoBehaviour
+{
+    [SerializeField] GameObject doorA;
+    [SerializeField] GameObject doorB;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //listen for a successful purchase from GM
+    //door A move -90 deg y
+    //door B move +90 deg y
+}
